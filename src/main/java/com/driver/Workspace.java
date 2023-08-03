0a55be222aede0709b6730179ea295c8a01ae2a0
Gowrishankar799 <130752694+Gowrishankar799@users.noreply.github.com>
2023-08-03 23:27:50 +0530

    kk

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,4 +1,4 @@
-package com.driver;
+/*package com.driver;
 
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -9,9 +9,13 @@ import java.util.Collections;
 public class Workspace extends Gmail{
 
     private ArrayList<Meeting> calendar; // Stores all the meetings
+    private int inboxcapacity;
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
+
+        calendar = new ArrayList<>();
+
 
     }
 
@@ -27,4 +31,49 @@ public class Workspace extends Gmail{
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
     }
+}*/
+package com.driver;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Workspace extends Gmail{
+
+    private ArrayList<Meeting> calendar; // Stores all the meetings
+
+    public Workspace(String emailId) {
+        // The inboxCapacity is equal to the maximum value an integer can store.
+        super(emailId,Integer.MAX_VALUE);
+        calendar=new ArrayList<>();
+
+    }
+
+    public void addMeeting(Meeting meeting){
+        //add the meeting to calendar
+        calendar.add(meeting);
+    }
+
+    public int findMaxMeetings(){
+        // find the maximum number of meetings you can attend
+        // 1. At a particular time, you can be present in at most one meeting
+        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
+        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        calendar.sort((a, b) -> a.getEndTime().compareTo(b.getEndTime()));
+        int ans=0;
+        LocalTime currentTime = LocalTime.of(0, 0); // Start from time 00:00
+
+        for (Meeting meeting : calendar) {
+            // If the current meeting's start time is after the current time,
+            // it means we can attend it without any conflicts.
+            if (meeting.getStartTime().compareTo(currentTime) >= 0) {
+                ans++;
+                currentTime = meeting.getEndTime();
+            }
+        }
+        return ans;
+    }
 }
+
