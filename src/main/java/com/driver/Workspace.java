9a4c852415a44d3423fdcdae8af3e7b7f8f9ce15
Gowrishankar799 <130752694+Gowrishankar799@users.noreply.github.com>
2023-08-03 23:40:12 +0530

    ll

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,4 +1,4 @@
-/*package com.driver;
+package com.driver;
 
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -13,6 +13,8 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
+        super(emailId,Integer.MAX_VALUE);
+        calendar=new ArrayList<>();
 
         calendar = new ArrayList<>();
 
@@ -21,6 +23,7 @@ public class Workspace extends Gmail{
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
+        calendar.add(meeting);
 
     }
 
@@ -29,12 +32,27 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        calendar.sort((a, b) -> a.getEndTime().compareTo(b.getEndTime()));
+        int ans=0;
+        LocalTime currentTime = LocalTime.of(0, 0); // Start from time 00:00
+
+        for (Meeting meeting : calendar) {
+            // If the current meeting's start time is after the current time,
+            // it means we can attend it without any conflicts.
+            if (meeting.getStartTime().compareTo(currentTime) >= 0) {
+                ans++;
+                currentTime = meeting.getEndTime();
+            }
+        }
+        return ans;
+    }
 
     }
-}*/
-package com.driver;
+
+/*package com.driver;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.
 
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -75,5 +93,5 @@ public class Workspace extends Gmail{
         }
         return ans;
     }
-}
+}*/
 
